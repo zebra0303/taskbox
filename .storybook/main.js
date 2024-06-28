4f7c28de3c68ce2ba27f40e80bca39f6dc1ef1a3
Larry Kim <larry@lunit.io>
2024-06-28 09:24:52 +0900

    add Task component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,25 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/components/**/*.stories.@(js|jsx)'],
+  staticDirs: ['../public'],
+
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,6 +11,8 @@ const preview = {
       },
     },
   },
+
+  tags: ['autodocs']
 };
 
 export default preview;
